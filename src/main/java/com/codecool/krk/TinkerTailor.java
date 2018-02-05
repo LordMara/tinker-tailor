677eea5300ce2c2a7fd8fca63507481208e41488
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 22:38:35 +0100

    Implement logic of getOutcome() method

--- src/main/java/com/codecool/krk/TinkerTailor.java
+++ src/main/java/com/codecool/krk/TinkerTailor.java
@@ -1,12 +1,38 @@
 package com.codecool.krk;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TinkerTailor {
     public List<Integer> getOutcome(Integer n, Integer k) {
         List<Integer> numberSequence = createSequence(n);
         List<Integer> outcome = new LinkedList<>();
+
+        while (numberSequence.size() > 1) {
+            int count = 0;
+
+            while (count < k) {
+                Iterator<Integer> iterator = numberSequence.iterator();
+                Queue<Integer> temp = new LinkedList<>();
+
+                while (iterator.hasNext()) {
+                    Integer number = iterator.next();
+                    count ++;
+                    iterator.remove();
+
+                    if (count < k) {
+                        temp.add(number);
+                    } else if (count == k) {
+                        outcome.add(number);
+                        break;
+                    }
+                }
+                numberSequence.addAll(temp);
+            }
+        }
+        outcome.add(numberSequence.get(0));
 
         return outcome;
     }
