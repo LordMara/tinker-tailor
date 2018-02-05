3dedbb52ebac849ba58f1341822f71243bf569b9
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-02-05 22:38:03 +0100

    Declare method getOutcome() and implement method createSequence()

--- src/main/java/com/codecool/krk/TinkerTailor.java
+++ src/main/java/com/codecool/krk/TinkerTailor.java
@@ -1,5 +1,22 @@
 package com.codecool.krk;
 
-public class TinkerTailor {
+import java.util.LinkedList;
+import java.util.List;
 
+public class TinkerTailor {
+    public List<Integer> getOutcome(Integer n, Integer k) {
+        List<Integer> numberSequence = createSequence(n);
+        List<Integer> outcome = new LinkedList<>();
+
+        return outcome;
+    }
+
+    private List<Integer> createSequence(Integer n) {
+        List<Integer> numberSequence = new LinkedList<>();
+        for (int i = 1; i < n + 1; i++) {
+            numberSequence.add(i);
+        }
+
+        return numberSequence;
+    }
 }
